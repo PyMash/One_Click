9a45fca26670318ac4e4d79bbd83c6de37213fdd
SpydyMasu <mashudahmedtalukdar@gmail.com>
2022-12-23 22:15:43 +0530

    just for test

--- lib/Pages/dt.dart
+++ lib/Pages/dt.dart
@@ -1,20 +1,6 @@
 void main(List<String> args) {
-  outcome(number) {
-    if (number == 0) {
-      return "Tutor";
-    } else if (number == 1) {
-      return "Electrician";
-    } else if (number == 2) {
-      return "Plumber";
-    } else if (number == 3) {
-      return "Mechanic";
-    } else if (number == 4) {
-      return "Assistance";
-    } else if (number == 5) {
-      return "Driver";
-    }
-  }
+  DateTime now = DateTime.now();
 
-  var data = outcome(1);
-  print(data);
+  int currentTime = int.parse(now.hour.toString());
+  print(currentTime);
 }
